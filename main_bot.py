a7d2acbbbe46946325bb7023f0b726d92f63058a
DConvoy <carvenkaor@gmail.com>
2018-07-17 04:28:09 +0700

    almost all, just correct logs

--- main_bot.py
+++ main_bot.py
@@ -5,13 +5,25 @@ from x_o_game import XOGame
 from guess_game import Guess
 import random
 import datetime
-from collections import OrderedDict
-from parser import Parser
+from parser_news import Parser
+
+
+def handler_answer(key, answer):
+    if '-' in key:
+        key1, key2 = key.split('-')
+        if key1 in answer and key2 in answer.lower():
+            return 1
+    else:
+        if key in answer.lower():
+            return 1
+        else:
+            return 0
 
 
 class State(metaclass=ABCMeta):
-    # def __init__(self, Context):
-    #     self.context = Context()
+    @abstractmethod
+    def set_state(self, context):
+        pass
 
     @abstractmethod
     def parser_answers(self):
@@ -26,35 +38,56 @@ class State(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def to_parse_news(self):
+    def to_parse_news(self, name):
         pass
 
     @abstractmethod
-    def to_parse_game_news(self):
+    def execution_state(self, context):
         pass
 
 
 class GameState(State):
-    def __init__(self, joke_phrases, answer_phrase, percent_wins):
-        self.name_game = ''
-        self.jokes = joke_phrases
-        self.answ_change_state = answer_phrase
-        self.percent_wins = percent_wins
+    def __init__(self, name_game):
+        self.name_game = name_game
+        self.list_states = [self.to_x_o_game, self.to_guess_game, self.to_parse_news]
+
+    def set_state(self, context):
+        self._context = context
+        self.jokes = self._context.settings.get('joke_phrases')
+        self.percent_wins = self._context.settings.get('percent_of_wins_bot')
+        self.execution_state('')
+        self.parser_answers()
 
     def parser_answers(self):
-        pass
+        print(self._context.settings.get('to_continue'))
+        answer = input()
+        for func in self.list_states:
+            keys = self._context.settings.get('words_to_parse_answ').get(func.__name__)
+            to_state = handler_answer(keys, answer)
+            if func.__name__ == 'to_parse_news' and to_state:
+                index_beg_name = answer.index('\'')
+                index_end_name = answer.rindex('\'')
+                namegame = answer[index_beg_name + 1: index_end_name]
+                func(namegame)
+            else:
+                if to_state:
+                    func()
+        self._context.change_state(ErrorState())
 
     def to_x_o_game(self):
-        pass
+        self._context.change_state(XOGameState('x_o_game'))
 
     def to_guess_game(self):
-        pass
+        self._context.change_state(GuessGameState('guess_game'))
 
-    def to_parse_news(self):
-        pass
+    def to_parse_news(self, name):
+        self._context.change_state(ParserState(name))
 
-    def to_parse_game_news(self):
-        pass
+    def execution_state(self, context):
+        self.make_joke()
+        result = self.start_game()
+        self.write_result(result)
+        self.parser_answers()
 
     def make_joke(self):
         '''Бот шутливо предлагает отказаться от игры, если до этого он побеждал
@@ -68,11 +101,8 @@ class GameState(State):
                 print(curr_joke_phrase)
                 #  Да - вызвать метод игры, нет, вернуться в инициализацию
                 answer = input()
-                reason = self.parser_answers(answer)
-                if reason:
-                    self.start_game()
-                # elif: переход к инициализации
-                # else: переход в ошибке
+                if answer.lower() != 'да':
+                    self._context.change_state(InitialState())
 
     def start_game(self):
         pass
@@ -135,67 +165,146 @@ class XOGameState(GameState):
         game = XOGame()
         winner = game.game()
         if winner == 'human':
-            self.write_result('human')
+            return 'human'
         elif winner == 'comp':
-            self.write_result('comp')
+            return 'comp'
 
 
 class GuessGameState(GameState):
     def start_game(self):
-        game = Guess()
+        dict_game = {'what_guess': self._context.settings.get('what_guess'),
+                     'list_guess': self._context.settings.get('list_guess')}
+        game = Guess(dict_game)
         winner = game.game()
         if winner == 'human':
-            self.write_result('human')
+            return 'human'
         elif winner == 'comp':
-            self.write_result('comp')
+            return 'comp'
 
 
 class ParserState(State):
-    def __init__(self, answer_phrase):
-        self.answ_change_state = answer_phrase
+    def __init__(self, name_game):
+        self.name_game = name_game
         self.curr_time = datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
+        self.type_r = 'news'
+        self.list_states = [self.to_x_o_game, self.to_guess_game, self.to_parse_news]
+
+    def set_state(self, context):
+        self._context = context
+        url = self._context.settings.get('base_url_news')
+        self.execution_state(url)
+        self.parser_answers()
 
     def parser_answers(self):
-        #  Отсюда прислать и урл, и название игры
-        pass
+        print(self._context.settings.get('to_continue'))
+        answer = input()
+        for func in self.list_states:
+            keys = self._context.settings.get('words_to_parse_answ').get(func.__name__)
+            to_state = handler_answer(keys, answer)
+            if func.__name__ == 'to_parse_news' and to_state:
+                index_beg_name = answer.index('\'')
+                index_end_name = answer.rindex('\'')
+                namegame = answer[index_beg_name + 1: index_end_name]
+                func(namegame)
+            else:
+                if to_state:
+                    func()
+        self._context.change_state(ErrorState())
 
     def to_x_o_game(self):
-        pass
+        self._context.change_state(XOGameState('x_o_game'))
 
     def to_guess_game(self):
-        pass
+        self._context.change_state(GuessGameState('guess_game'))
 
-    def to_parse_news(self):
-        pass
+    def to_parse_news(self, name):
+        self._context.change_state(ParserState(name))
 
-    def to_parse_game_news(self):
-        pass
+    def execution_state(self, context):
+        self.check_time()
+        self.parse(context)
+        self.parser_answers()
 
-    def get_time_request(self, type_req):
+    def check_time(self):
+        time_log = self.get_time_request()
+        if time_log is not None:
+            date, time = time_log.split(' - ')[1].split(': ')
+            curr_date, curr_time = self.curr_time.split(' - ')[1].split(': ')
+
+            eq = self.eq_date(date, curr_date)
+            if eq == 'same':
+                fr = int(self._context.settings.get('frequency_req_min'))
+                eq_t = self.eq_time(time, curr_time, fr)
+                if eq_t == 'same':
+                    print('Извините, вы не можете пока отправлять запросы на новости.'
+                          ' Повторите через ' + str(fr) + ' минут.')
+                    self._context.change_state(InitialState())
+
+    def eq_time(self, time, curr_time, fr):
+        h, m = time.split(':')
+        h, m, = int(h), int(m)
+        curr_h, curr_m = curr_time.split(':')
+        curr_h, curr_m, = int(curr_h), int(curr_m)
+        # От текущего времени отнимаем необходимый "отступ" для сверения
+        if fr > 60:
+            tmp_h = fr // 60
+            curr_h -= tmp_h
+            tmp_m = fr % 60
+            if (curr_m - tmp_m) < 0:
+                curr_h -= 1
+                curr_m = 60 + (curr_m - tmp_m)
+        else:
+            if (curr_m - fr) < 0:
+                curr_h -= 1
+                curr_m = 60 + (curr_m - fr)
+            else:
+                curr_m -= fr
+
+        if curr_h > h:
+            return 'no'
+        elif curr_h == h:
+            if curr_m > m:
+                return 'no'
+        else:
+            return 'same'
+
+    def eq_date(self, date, curr_date):
+        # проверка совпадения дат
+        d, m, y = date.split('-')
+        d, m, y = int(d), int(m), int(y)
+        curr_d, curr_m, curr_y = curr_date.split('-')
+        curr_d, curr_m, curr_y = int(curr_d), int(curr_m), int(curr_y)
+        if y == curr_y:
+            if m == curr_m:
+                if d == curr_d:
+                    return 'same'
+        return 'no'
+
+    def get_time_request(self):
         with open('parser_log.txt', 'r', encoding='utf-8') as log:
             logs = log.read().split('\n')
+            if self.name_game == '':
+                tmp = self.type_r
+            else:
+                tmp = self.type_r + " " + self.name_game
             for l in logs:
-                if type_req in l:
+                if tmp in l:
                     return l
             return None
 
-    def parse(self, base_url, name_game):
-        type = ''
-        if name_game == '':
-            type = 'news'
-        else:
-            type = 'news_about_game'
-        pars = Parser(base_url, name_game)
+    def parse(self, base_url):
+        pars = Parser(base_url, self.name_game)
         news = pars.get_news()
         for key, value in news.items():
             print('Заголовок: ' + value)
             print('Ссылка: ' + key)
             print('\n')
-        self.write_time(type)
+        self.write_time()
 
-    def write_time(self, type):
-        time = self.get_time_request(type)
-        str_time = 'type: ' + type + 'time: ' + self.curr_time
+    def write_time(self):
+        time = self.get_time_request()
+        str_time = 'type: ' + self.type_r + 'time: ' + self.curr_time
+
         if time is None:
             with open('parser_log.txt', 'a', encoding='utf-8') as log:
                 log.write(str_time + '\n')
@@ -211,64 +320,111 @@ class ParserState(State):
                         log.write(l + '\n')
 
 
-class ParserNewsState(ParserState):
-    pass
-
-
-class ParserNewsGameState(ParserState):
-    pass
-
-
 class InitialState(State):
-    def __init__(self, answer_phrase):
-        self.answ_change_state = answer_phrase
+    def __init__(self):
+        self.list_states = [self.to_x_o_game, self.to_guess_game, self.to_parse_news]
+
+    def set_state(self, context):
+        self._context = context
+        self.execution_state('')
 
     def parser_answers(self):
-        pass
-    # надо возвратить новый объект состояния????
+        print(self._context.settings.get('to_continue'))
+        answer = input()
+        for func in self.list_states:
+            keys = self._context.settings.get('words_to_parse_answ').get(func.__name__)
+            to_state = handler_answer(keys, answer)
+            if func.__name__ == 'to_parse_news' and to_state:
+                namegame = ''
+                try:
+                    index_beg_name = answer.index('"')
+                    index_end_name = answer.rindex('"')
+                    namegame = answer[index_beg_name + 1: index_end_name]
+                except:
+                    func('')
+                else:
+                    func(namegame)
+            else:
+                if to_state:
+                    func()
+        self._context.change_state(ErrorState())
+
+    def execution_state(self, context):
+        if self._context.initial_first:
+            curr_intro = random.choice(self._context.settings.get('introduction_var'))
+            print(curr_intro)
+            for opport in self._context.settings.get('introduction_bot_opport'):
+                print(opport)
+        else:
+            print(self._context.settings.get('introduction_again'))
+            for opport in self._context.settings.get('introduction_bot_opport'):
+                print(opport)
+        self.parser_answers()
 
     def to_x_o_game(self):
-        pass
+        self._context.change_state(XOGameState('x_o_game'))
 
     def to_guess_game(self):
-        pass
+        self._context.change_state(GuessGameState('guess_game'))
 
-    def to_parse_news(self):
-        pass
-
-    def to_parse_game_news(self):
-        pass
+    def to_parse_news(self, name):
+        self._context.change_state(ParserState(name))
 
 
 class ErrorState(State):
-    def __init__(self, answer_phrase):
-        self.answ_change_state = answer_phrase
+    def __init__(self):
+        self.list_states = [self.to_x_o_game, self.to_guess_game, self.to_parse_news]
+
+    def set_state(self, context):
+        self._context = context
+        self.execution_state('')
 
     def parser_answers(self):
-        pass
+        print(self._context.settings.get('to_continue'))
+        answer = input()
+        for func in self.list_states:
+            keys = self._context.settings.get('words_to_parse_answ').get(func.__name__)
+            to_state = handler_answer(keys, answer)
+            if func.__name__ == 'to_parse_news' and to_state:
+                namegame = ''
+                try:
+                    index_beg_name = answer.index('"')
+                    index_end_name = answer.rindex('"')
+                    namegame = answer[index_beg_name + 1: index_end_name]
+                except:
+                    func('')
+                else:
+                    func(namegame)
+            else:
+                if to_state:
+                    func()
+        self._context.change_state(ErrorState())
+
+    def execution_state(self, context):
+        print(self._context.settings.get('error_phrase'))
+        self.parser_answers()
 
     def to_x_o_game(self):
-        pass
+        self._context.change_state(XOGameState('x_o_game'))
 
     def to_guess_game(self):
-        pass
+        self._context.change_state(GuessGameState('guess_game'))
 
-    def to_parse_news(self):
-        pass
-
-    def to_parse_game_news(self):
-        pass
+    def to_parse_news(self, name):
+        self._context.change_state(ParserState(name))
 
 
 class Context:
     def __init__(self, file_set):
         self.path_file_settings = file_set
         self.settings = {}
+        self.get_settings()
         self.initial_first = True
-        self._state = InitialState()
+        self._state = self.change_state(InitialState())
 
-    def change_state(self):
-        self._state.parser_answers()
+    def change_state(self, new_state):
+        self._state = new_state
+        self._state.set_state(self)
 
     def get_settings(self):
         with open(self.path_file_settings, 'r', encoding='utf-8',) as f:
@@ -276,9 +432,22 @@ class Context:
             self.settings = data
 
 
+    # def to_x_o_game(self):
+    #     self._state.to_x_o_game()
+    #
+    # def to_guess_game(self):
+    #     self._state.to_guess_game()
+    #
+    # def to_parse_news(self):
+    #     self._state.to_parse_news()
+    #
+    # def to_parse_game_news(self):
+    #     self._state.to_parse_game_news()
+
+
 if __name__ == '__main__':
-    g = Context(1, 'settings.json')
-    g.get_settings()
+    g = Context('settings.json')
+    print('finish')
 
 
 
